921283aa5d9135050f07023b904e6252acfb1bb9
PROMEZA <luisianavcc@gmail.com>
2026-06-25 16:13:32 -0500

    v139: deploy bundle with junk-contact cleanup migration

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = "promeza-v137";
+const CACHE = "promeza-v139";
 const ASSETS = [
   "./styles.css",
   "./i18n.js", "./airtable.js",
-  "./bundle.137.js", "./msal-browser.min.js",
+  "./bundle.139.js", "./msal-browser.min.js",
   "./data_churches.js",
 ];
 
