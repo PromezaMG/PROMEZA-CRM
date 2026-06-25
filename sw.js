3e27fdaf9e7e98c8e078494a7c84de22333ed3c6
PROMEZA <luisianavcc@gmail.com>
2026-06-25 12:23:44 -0500

    v136: deploy bundle with Entidades pagination + Directorio 'Ver más'

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = "promeza-v135";
+const CACHE = "promeza-v136";
 const ASSETS = [
   "./styles.css",
   "./i18n.js", "./airtable.js",
-  "./bundle.135.js", "./msal-browser.min.js",
+  "./bundle.136.js", "./msal-browser.min.js",
   "./data_churches.js",
 ];
 
