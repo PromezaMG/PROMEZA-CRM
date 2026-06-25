8f44d9d9e2870120a716eb84c59453d97dde9c58
PROMEZA <luisianavcc@gmail.com>
2026-06-25 11:44:13 -0500

    v134: deploy bundle with persistent filters (bundle.134.js)

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = "promeza-v133";
+const CACHE = "promeza-v134";
 const ASSETS = [
   "./styles.css",
   "./i18n.js", "./airtable.js",
-  "./bundle.133.js", "./msal-browser.min.js",
+  "./bundle.134.js", "./msal-browser.min.js",
   "./data_churches.js",
 ];
 
