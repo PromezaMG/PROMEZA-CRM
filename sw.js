0c5d640555493251a7394f117162fafd5d746844
PROMEZA <luisianavcc@gmail.com>
2026-06-26 11:53:57 -0500

    v145: point index/sw to bundle.145.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = "promeza-v143";
+const CACHE = "promeza-v145";
 const ASSETS = [
   "./styles.css",
   "./i18n.js", "./airtable.js",
-  "./bundle.143.js", "./msal-browser.min.js",
+  "./bundle.145.js", "./msal-browser.min.js",
   "./data_churches.js",
 ];
 
